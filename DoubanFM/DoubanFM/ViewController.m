cc27df3fe078d35195e32788d413ac18e4a21f11
lkjy <lkjy@kanghaolongdemac-mini.local>
2016-04-28 10:40:43 +0800

    创建

---
+++ DoubanFM/DoubanFM/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DoubanFM
+//
+//  Created by lkjy on 16/4/28.
+//  Copyright © 2016年 康康. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    
+    NSLog(@"康");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
